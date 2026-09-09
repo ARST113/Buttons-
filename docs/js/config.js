6b8f8974a8dc00a312bf7f947e88185ca8071275
ARST113 <96774211+ARST113@users.noreply.github.com>
2026-09-09 16:41:08 +0300

    chore: keep frontend API config in app entrypoint

--- docs/js/config.js
+++
@@ -1,5 +0,0 @@
-const localHosts = new Set(['localhost', '127.0.0.1']);
-
-export const API_BASE = localHosts.has(window.location.hostname)
-  ? 'http://127.0.0.1:8765'
-  : 'https://arxistar.duckdns.org/food-api';
